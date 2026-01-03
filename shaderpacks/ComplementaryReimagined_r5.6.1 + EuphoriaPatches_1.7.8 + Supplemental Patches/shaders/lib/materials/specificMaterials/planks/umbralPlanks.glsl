794c01854c196cba5c204e3b1a7971df6878f158
Uvivgflv <122619968+Uvivgflv@users.noreply.github.com>
2026-01-04 01:14:39 +0500

    Delete shaderpacks directory
    no need

--- shaderpacks/ComplementaryReimagined_r5.6.1 + EuphoriaPatches_1.7.8 + Supplemental Patches/shaders/lib/materials/specificMaterials/planks/umbralPlanks.glsl
+++
@@ -1,6 +0,0 @@
-smoothnessG = pow2(color.b) * 0.35;
-smoothnessD = smoothnessG;
-
-#ifdef COATED_TEXTURES
-    noiseFactor = 0.66;
-#endif
--- shaderpacks/ComplementaryReimagined_r5.6.1 + EuphoriaPatches_1.7.8 + Supplemental Patches/shaders/lib/materials/specificMaterials/terrain/sulfurBlock.glsl
+++
@@ -1,6 +0,0 @@
-smoothnessG = 0.5 * pow2(color.r);
-smoothnessD = smoothnessG;
-
-#ifdef COATED_TEXTURES
-    noiseFactor = 0.8;
-#endif
